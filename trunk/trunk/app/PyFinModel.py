2a39a175c693ffb7b524477bdd8789dc151b44d2
RamneekHanda <RamneekHanda@3330e35f-c857-f6fc-2494-acd8daffd0e6>
2011-05-02 22:54:57 +0000

    initial checkin

---
+++ trunk/trunk/app/PyFinModel.py
@@ -0,0 +1,70 @@
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib import font_manager
+import matplotlib as mpl
+
+class ModelProperties:
+  def __init__(self, list):
+    self.list = list
+    self.keyval = {}
+    for x in list:
+      self.keyval[x[0]] = x[1]
+
+  def getPropertyList(self):
+    return self.list
+
+  def getPropertyValue(self, key):
+    try:
+      return self.keyval[key] 
+    except KeyError:
+      raise NameError
+
+  def setPropertyValue(self, key, val):
+    self.keyval[key] = val
+    pass
+
+class PyFinChart(FigureCanvas):
+  def __init__(self):
+    mpl.rcParams['font.size'] = 6
+    mpl.rcParams['axes.grid'] = True
+    self.fig = Figure(figsize=(3, 3), dpi=100)
+    FigureCanvas.__init__(self, self.fig)
+    self.setSizePolicy(QtGui.QSizePolicy.Expanding,
+                       QtGui.QSizePolicy.Expanding)
+    self.axes = self.fig.add_subplot(111)
+    self.axes.set_aspect('equal')
+    FigureCanvas.updateGeometry(self)
+
+  def addLinePlot(self, xval, yval):
+    self.axes.plot(xval, yval)
+
+class PyFinModel:
+  def __init__(self, appWindow):
+    self.appWindow = appWindow
+
+  def raiseABCError():
+    raise "Abstract Method Invoked!"
+
+  def getModelName():
+    raiseABCError
+
+  def getModelLink():
+    raiseABCError
+
+  def getProperties(self):
+    raiseABCError
+
+  def getActions(self):
+    raiseABCError
+
+  def show1DData(self, name, array):
+    self.appWindow.add1DData(name, array)
+
+  def show2DData(self, name, array):
+    self.appWindow.add2DData(name, array)
+
+  def addGraph(self, name, pyFinChart):
+    self.appWindow.add2DGraph(name, pyFinChart)
+
+  def notifyState(self, pct, text):
+    raiseABCError
---
+++ trunk/trunk/models/SimpleRandomWalk.py
@@ -0,0 +1,66 @@
+from PyFinModel import PyFinModel, ModelProperties
+from functools import partial as functools_partial
+from math import exp, sqrt
+from numpy import zeros, flipud
+
+## 
+## 
+class SimpleRandomWalk(PyFinModel):
+  def __init__(self, data):
+    self.properties = ModelProperties([["s0", 100, "Spot Price"], ["r", 0.06, "Interest Rate"], 
+                                       ["v", 0.2, "Volatility"], ["t", 1, "Time to Maturity"], 
+                                       ["k", 100, "Strike Price"], ["n", 3, "Number of steps in the Binomial tree"]])
+    PyFinModel.__init__(self, data)
+  
+  def getProperties(self):
+    return self.properties.getPropertyList()
+  
+  def getModelName():
+    return "Binomial Tree Model"
+  
+  def getModelLink():
+    return "http://www.google.com"
+  
+  def getActions(self):
+    runModel = functools_partial(SimpleRandomWalk.runModel, self)
+    return {"Run Model": runModel}
+  
+  def setupProperties(self, prop):
+    for x in prop.keyval:
+      self.__dict__[x] = prop.keyval[x]
+      
+    
+  def runModel(self, prop):
+    self.setupProperties(self.properties)
+
+    dt = float(self.t)/self.n
+    nu = self.r - 0.5 * pow(self.v, 2)
+    dxu = sqrt(pow(self.v, 2) * dt + pow((nu * dt), 2))
+    dxd = -dxu
+    pu = 0.0
+    pu = round(0.5 + 0.5 * nu * dt/dxu, 4)
+    pd = round(1 - pu, 4)
+    
+    disc = round(exp(-self.r * dt), 4)
+    nodes = self.n + 1
+    
+    ##initialize asset prices at maturity
+    st = zeros(nodes)
+    
+    st[0] = self.s0 * exp(self.n * dxd)
+    for i in range(1, nodes):
+      st[i] = st[i-1] * exp(dxu - dxd)
+    
+    ##initialize options value at maturity
+    C = zeros((nodes, nodes))
+    for i in range(0, nodes):
+      C[i, self.n] = max(0.0, st[i] - self.k)
+    
+    ##step back in the tree
+    for timestep in range (self.n - 1, -2, -1):
+      for node in range(0, timestep + 1):
+        C[node, timestep] = disc * ((pu * C[node + 1, timestep + 1]) + (pd * C[node, timestep + 1]))
+        
+    C = flipud(C)
+    self.show2DData("European Call Option Prices", C)
+    
