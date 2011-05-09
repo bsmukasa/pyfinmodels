cf9e3548c3166ff36eef9da49d2fb364a6975044
RamneekHanda <RamneekHanda@3330e35f-c857-f6fc-2494-acd8daffd0e6>
2011-05-09 18:36:02 +0000

    now the properties get updated before running the model

--- app/PyFinModelsMain.py
+++ app/PyFinModelsMain.py
@@ -2,10 +2,20 @@ import sys
 from PyQt4 import QtGui, QtCore
 from MainWindow import Ui_MainWindow
 from models.BinomialTreeModel import BinomialTreeModel
+from functools import partial as functools_partial
 
+class QPropertyVal(QtGui.QTableWidgetItem):
+  def __init__(self, text):
+    QtGui.QTableWidgetItem.__init__(self, text)
 
-class PyFinModelsApp(QtGui.QMainWindow):
-  def __init__(self):
+  def setAssociatedProperty(self, name):
+    self.name = name
+
+  def associatedProperty(self):
+    return self.name
+
+class PyFinModelsApp(QtGui.QMainWindow) :
+  def __init__(self) :
     QtGui.QMainWindow.__init__(self)
     self.ui = Ui_MainWindow()
     self.ui.setupUi(self)
@@ -13,96 +23,110 @@ class PyFinModelsApp(QtGui.QMainWindow):
     self.chartcol = 0
     self.ui.frmNotify.hide()
     self.sheetrow = 0
+    self.propertyChanged = functools_partial(PyFinModelsApp.propertyChangeEvent, self)
+    self.connect(self.ui.tblSheet, QtCore.SIGNAL('itemChanged(QTableWidgetItem *)'), self.propertyChangeEvent)
 
-  def getSheet(self):
+  def propertyChangeEvent(self, item):
+    if isinstance(item, QPropertyVal):
+      self.modelProperties.setPropVal(item.associatedProperty(), item.text())
+  
+  def getSheet(self) :
     return self.ui.tblSheet
 
-  def getCodeEditor(self):
+  def getCodeEditor(self) :
     return self.ui.txtCode
 
-  def getModelHintsTextbox(self):
+  def getModelHintsTextbox(self) :
     return self.ui.txtModelHints
 
-  def addProperties(self, propList):
+  def addProperties(self, mprops) :
     col = 0
-    for x in propList:
-      itemKey = QtGui.QTableWidgetItem(x[0])
-      itemKey.setBackground(QtGui.QBrush(QtGui.QColor(200, 200, 200)))
+    self.modelProperties = mprops
+    props = mprops.getProps()
+    for key in props :
+      itemKey = QtGui.QTableWidgetItem(key)
+      itemKey.setBackground(QtGui.QBrush(QtGui.QColor('Gray')))
       fnt = itemKey.font()
-      itemKey.setToolTip(x[2])
+      itemKey.setToolTip(mprops.getPropHelp(key))
       fnt.setWeight(QtGui.QFont.Bold)
       itemKey.setFont(fnt)
       itemKey.setFlags(QtCore.Qt.ItemIsEnabled)
       self.getSheet().setItem(self.sheetrow, col, itemKey)
 
-      itemVal = QtGui.QTableWidgetItem(str(x[1]))
-      itemVal.setBackground(QtGui.QBrush(QtGui.QColor(200, 50, 10)))
-      itemVal.setToolTip(x[2])
-      self.getSheet().setItem(self.sheetrow, col+1, itemVal)
+      itemVal = QPropertyVal(str(mprops.getPropVal(key)))
+      itemVal.setAssociatedProperty(key)
+      itemVal.setBackground(QtGui.QBrush(QtGui.QColor("Red")))
+      itemVal.setToolTip(mprops.getPropHelp(key))
+      self.getSheet().setItem(self.sheetrow, col + 1, itemVal)
       self.sheetrow += 1
 
-  def addModelActions(self, actionDict):
+  def addModelActions(self, actionDict) :
     col = 0
-
-    for x in actionDict:
+    for x in actionDict :
       btn = QtGui.QPushButton(x, self.getSheet())
       btn.setFont(self.getSheet().font())
-      self.connect(btn, QtCore.SIGNAL('clicked(bool)'), actionDict[x])
+      ahandler = functools_partial(PyFinModelsApp.actionHandler, self, actionDict[x])
+      self.connect(btn, QtCore.SIGNAL('clicked(bool)'), ahandler)
       self.getSheet().setCellWidget(self.sheetrow, col, btn)
-      col += 1        
+      col += 1
 
-  def add1DData(self, name, data):
+  def actionHandler(self, actionTarget):
+    actionTarget(self.modelProperties)
+    pass
+  
+  def add1DData(self, name, data) :
     self.sheetrow += 1
     col = 0
-    for x in data:
+    for x in data :
       item = QtGui.QTableWidgetItem(x)
-      item.setBackground(QtGui.QBrush(QtGui.QColor(0, 150, 150)))
+      item.setBackground(QtGui.QBrush(QtGui.QColor("Brown")))
       item.setFlags(QtCore.Qt.ItemIsEnabled)
       self.getSheet().setItem(self.sheetrow, col, item)
       col += 1
 
-  def showOutputHeading(self, name):
+  def showOutputHeading(self, name) :
     self.sheetrow += 1
     itemKey = QtGui.QTableWidgetItem(name)
-    itemKey.setBackground(QtGui.QBrush(QtGui.QColor(200, 100, 100)))
+    itemKey.setBackground(QtGui.QBrush(QtGui.QColor("Pink")))
     fnt = itemKey.font()
     fnt.setWeight(QtGui.QFont.Bold)
     itemKey.setFont(fnt)
     itemKey.setFlags(QtCore.Qt.ItemIsEnabled)
     self.getSheet().setItem(self.sheetrow, 0, itemKey)
 
-
-  def add2DData(self, name, data):
+  def add2DData(self, name, data) :
     self.showOutputHeading(name)
     col = 0
-    for x in data:
+    for x in data :
       self.sheetrow += 1
       col = 0
-      for y in x:
+      for y in x :
         item = QtGui.QTableWidgetItem(str(y))
         item.setBackground(QtGui.QBrush(QtGui.QColor(255, 100, 100)))
         item.setFlags(QtCore.Qt.ItemIsEnabled)
         self.getSheet().setItem(self.sheetrow, col, item)
         col += 1
 
-  def add2DChart(self, name, pyFinChart):
-    self.ui.chartGrid.addWidget(chart, self.chartrow, self.chartcol)
-    if (self.chartcol == 3):
+  def add2DChart(self, name, pyFinChart) :
+    self.ui.chartGrid.addWidget(pyFinChart, self.chartrow, self.chartcol)
+    if self.chartcol == 3 :
       self.chartrow += 1
       self.chartcol = 0
-    else:
+    else :
       self.chartcol += 1
 
-  def loadModel(self, model):
+  def loadModel(self, model) :
     self.getSheet().clearContents()
     self.addProperties(model.getProperties())
-    self.addModelActions(model.getActions())
+    self.addModelActions(model.getModelActions())
 
-
-if __name__ == "__main__":
+def main():
   app = QtGui.QApplication(sys.argv)
   window = PyFinModelsApp()
   model = BinomialTreeModel(window)
   window.loadModel(model)
   window.show()
   sys.exit(app.exec_())
+  
+if __name__ == "__main__" :
+  main()
